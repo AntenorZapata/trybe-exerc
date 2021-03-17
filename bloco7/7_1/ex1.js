dc7cfca274aebe93483ea7d14d8c95b36f8880e2
Antenor <antenorzapata@gmail.com>
2021-03-17 15:56:04 -0300

    ex6

--- bloco7/7_1/ex1.js
+++ bloco7/7_1/ex1.js
@@ -12,3 +12,4 @@ const testingScope = (escopo) => {
 };
 
 testingScope(false);
+
--- bloco7/7_1/ex6.js
+++ bloco7/7_1/ex6.js
@@ -1,17 +1,36 @@
+
 // exer 6
 
 const changeString = (str) => {
-  let result = 'Tryber x aqui!'.toLocaleLowerCase();
-  let arr = result.split(' ');
-  let frase = '';
-
-  for (let word of arr) {
-    if (word === 'x') {
-      word = str;
-      frase += `${word} `;
-    } else {
-      frase += `${word} `;
+    let result = 'Tryber x aqui!'.toLocaleLowerCase();
+    let arr = result.split(' ');
+    let frase = '';
+  
+    for (let word of arr) {
+      if (word === 'x') {
+        word = str;
+        frase += `${word} `;
+      } else {
+        frase += `${word} `;
+      }
     }
-  }
-  return frase;
-};
+    return frase;
+  };
+  
+  // Or
+  
+  const arr = ['Html', 'Css', 'Javascript', 'Python', 'React'];
+  
+  const changeString = (str) => {
+    let string = `Tryber, é ${str} aqui! `;
+  
+    let result = `${string}Essas são minhas skills: `;
+  
+    arr.forEach((i) => {
+      result += `    
+      ${i}`;
+    });
+    return `${result}
+  
+    #goTryber`;
+  };
