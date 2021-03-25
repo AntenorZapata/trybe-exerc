a127cbace4a059c731a96586fc15ccfaef935051
Antenor <antenorzapata@gmail.com>
2021-03-25 16:31:21 -0300

    ex1

---
+++ bloco8/8_4/ex1.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+function flatten() {
+  return arrays.reduce((result, item) => {
+    return result.concat(item);
+  }, []);
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
