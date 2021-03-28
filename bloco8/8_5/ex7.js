1d86d11a84600f420dcc8b662b00a5c1965a5962
Antenor <antenorzapata@gmail.com>
2021-03-28 20:40:38 -0300

    ex7

---
+++ bloco8/8_5/ex7.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+
+const ships = [
+  {
+    name: "Titanic",
+    length: 269.1,
+    measurementUnit: "meters",
+  },
+  {
+    name: "Queen Mary 2",
+    length: 1132,
+    measurementUnit: "feet",
+  },
+  {
+    name: "Yamato",
+    length: 256,
+    measurementUnit: "meters",
+  },
+];
+const shipLength = (obj) => {
+  const arr = obj.map((i) => {
+    return `${i.name} is ${i.length} ${i.measurementUnit} long`;
+  });
+  return arr;
+};
+
+// console.log(shipLength(ships));
+
+assert.strictEqual(shipLength(ships[0]), "Titanic is 269.1 meters long");
+assert.strictEqual(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long");
+assert.strictEqual(shipLength(ships[2]), "Yamato is 256 meters long");
